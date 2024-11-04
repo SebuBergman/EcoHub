c469e274f915aeacd6a10a19cdd4808103aa6d24
Sebastian Bergman <basti.bergman@proton.me>
2024-11-04 16:22:31 +0200

    fix: changed development environment to vite

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@config": path.resolve(__dirname, "./src/app/config"),
+      "@assets": path.resolve(__dirname, "./src/app/assets"),
+    },
+  },
+  plugins: [react()],
+});
