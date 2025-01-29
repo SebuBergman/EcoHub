dbf8b56594cb2fd9f8e53d0a1e1f3e8e13507760
Sebastian Bergman <basti.bergman@proton.me>
2025-01-29 19:39:38 +0200

    fix: mobile optimized the footer

--- src/features/footer/index.tsx
+++ src/features/footer/index.tsx
@@ -8,96 +8,154 @@ import {
 } from "react-icons/fa6";
 
 import "./styles.scss";
-import { Stack } from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { AppRoutes } from "@/app/config/routes";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <div className="footer_container">
-        <div className="footer_newsletter">
-          <div className="footer_newsletter_text">
-            <h3>Subscribe to our newsletter</h3>
-            <p>
+    <Box sx={{ bgcolor: "#f9fafb" }} mt={4} mb={4}>
+      <Stack sx={{ maxWidth: "1280px", width: "100%", margin: "auto" }}>
+        <Stack
+          sx={{
+            alignItems: "center",
+            flexDirection: { xs: "column", md: "row" },
+            padding: { xs: "20px", md: "48px 0px 48px 0px" },
+          }}
+        >
+          <Stack sx={{ flex: { md: 2 }, pb: { xs: 2 } }}>
+            <Typography variant="h6">Subscribe to our newsletter</Typography>
+            <Typography variant="subtitle1">
               Stay in touch and be part of the sustainability debate. Follow us
               on social media, subscribe to our newsletter and join our
               community.
-            </p>
-          </div>
-          <div className="footer_newsletter_input">
-            <input
+            </Typography>
+          </Stack>
+          <Stack sx={{ flex: { md: 2 }, flexDirection: "row", height: "58px" }}>
+            <TextField
               type="email"
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Enter email here"
+              fullWidth
             />
-            <button className="button_green">Tilaa</button>
-          </div>
-        </div>
-        <div className="footer_navlinks">
-          <div className="footer_navlinks_logo">
+            <Button
+              variant="contained"
+              sx={{
+                bgcolor: "#2e7d32",
+                ml: 1,
+                padding: { xs: "0px 15px", md: "0px 25px" },
+              }}
+            >
+              Tilaa
+            </Button>
+          </Stack>
+        </Stack>
+        <Stack
+          sx={{
+            flexDirection: { xs: "column", md: "row" },
+            padding: { xs: "0px 20px" },
+            gap: "10px",
+          }}
+        >
+          <Stack sx={{ flex: { md: 1 }, flexDirection: { xs: "column" } }}>
             {/*<Logo />*/}
-            <h3>EcoHub</h3>
-            <p>Together towards a more sustainable tomorrow.</p>
-          </div>
-          <div className="footer_navlinks_links">
-            <h4>Learn</h4>
-            <ul>
-              <li>
-                <a href={AppRoutes.envIssues}>Environmental Issues</a>
-              </li>
-              <li>
-                <a href={AppRoutes.sustainableLiving}>
-                  Sustainable Living Tips
-                </a>
-              </li>
-              <li>Green Innovations</li>
-            </ul>
-            <h4>Watch</h4>
-            <ul>
-              <li>Videos</li>
-              <li>Success Stories</li>
-            </ul>
-            <h4>Interact</h4>
-            <ul>
-              <li>Quizzes & Challenges</li>
-              <li>
-                <a href={AppRoutes.discussionForum}>Discussion Forum</a>
-              </li>
-            </ul>
-          </div>
-          <div className="footer_navlinks_links">
-            <h4>Resource</h4>
-            <ul>
-              <li>Downloads & Library</li>
-              <li>Carbon Footprint Tool</li>
-              <li>Environmental Impact Assessment</li>
-            </ul>
-            <h4>Engage</h4>
-            <ul>
-              <li>User Profiles</li>
-              <li>Signs & Achievements</li>
-              <li>Submit Your Story</li>
-            </ul>
-          </div>
-          <div className="footer_navlinks_links">
-            <h4>About</h4>
-            <ul>
-              <li>
-                <a href={AppRoutes.missionTeam}>Mission & Team</a>
-              </li>
-              <li>News & Updates</li>
-              <li>
-                <a href={AppRoutes.pressAndMedia}>Press & Media</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="footer_copyright_socials">
-          <div className="footer_copyright">
+            <Typography variant="h6">EcoHub</Typography>
+            <Typography variant="subtitle1">
+              Together towards a more sustainable tomorrow.
+            </Typography>
+          </Stack>
+          <Stack
+            sx={{ flex: { md: 2 }, flexDirection: { xs: "column", md: "row" } }}
+            gap={1}
+          >
+            <Stack sx={{ flex: { md: 1 }, ml: { xs: 0, md: "5px" } }} gap={1}>
+              <Stack>
+                <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                  Learn
+                </Typography>
+                <ul className="footer_ul">
+                  <li>
+                    <a href={AppRoutes.envIssues}>Environmental Issues</a>
+                  </li>
+                  <li>
+                    <a href={AppRoutes.sustainableLiving}>
+                      Sustainable Living Tips
+                    </a>
+                  </li>
+                  <li>Green Innovations</li>
+                </ul>
+              </Stack>
+              <Stack>
+                <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                  Watch
+                </Typography>
+
+                <ul className="footer_ul">
+                  <li>Videos</li>
+                  <li>Success Stories</li>
+                </ul>
+              </Stack>
+              <Stack>
+                <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                  Interact
+                </Typography>
+
+                <ul className="footer_ul">
+                  <li>Quizzes & Challenges</li>
+                  <li>
+                    <a href={AppRoutes.discussionForum}>Discussion Forum</a>
+                  </li>
+                </ul>
+              </Stack>
+            </Stack>
+            <Stack sx={{ flex: { md: 1 }, ml: { xs: 0, md: "5px" } }} gap={1}>
+              <Stack>
+                <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                  Resource
+                </Typography>
+                <ul className="footer_ul">
+                  <li>Downloads & Library</li>
+                  <li>Carbon Footprint Tool</li>
+                  <li>Environmental Impact Assessment</li>
+                </ul>
+              </Stack>
+              <Stack>
+                <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                  Engage
+                </Typography>
+                <ul className="footer_ul">
+                  <li>User Profiles</li>
+                  <li>Signs & Achievements</li>
+                  <li>Submit Your Story</li>
+                </ul>
+              </Stack>
+            </Stack>
+            <Stack sx={{ flex: { md: 1 }, ml: { xs: 0, md: "5px" } }}>
+              <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                About
+              </Typography>
+              <ul className="footer_ul">
+                <li>
+                  <a href={AppRoutes.missionTeam}>Mission & Team</a>
+                </li>
+                <li>News & Updates</li>
+                <li>
+                  <a href={AppRoutes.pressAndMedia}>Press & Media</a>
+                </li>
+              </ul>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Stack
+          sx={{ p: { xs: "25px 20px", md: "0px 48px" }, flexDirection: {xs: "column", md: "row"}, alignItems: {xs: "center"} }} gap={1}
+        >
+          <Stack sx={{ flex: 1, flexWrap: "flex-start" }}>
             <p>© 2024 EcoHub.</p>
-          </div>
-          <Stack className="footer_socials" flexDirection={"row"}>
+          </Stack>
+          <Stack
+            sx={{ flex: 2, flexWrap: "flex-end", flexDirection: "row" }}
+            gap={1}
+          >
             <FaInstagram size={24} />
             <FaFacebook size={24} />
             <FaLinkedin size={24} />
@@ -105,8 +163,8 @@ export default function Footer() {
             <FaYoutube size={24} />
             <FaGithub size={24} />
           </Stack>
-        </div>
-      </div>
-    </div>
+        </Stack>
+      </Stack>
+    </Box>
   );
 }
