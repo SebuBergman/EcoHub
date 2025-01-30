de659987bb886f8445acf405a6eb16e7f7bb6f28
Sebastian Bergman <basti.bergman@proton.me>
2025-01-30 19:37:29 +0200

    feat: made not-found 404 page better

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,12 +1,31 @@
+import Footer from "@/features/footer";
+import Header from "@/features/header";
+import { Box, Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+import pageNotFound from "@assets/images/page_not_found.webp";
 
 export default function NotFoundPage() {
   return (
     <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
+      <Header />
+      <Stack
+        sx={{ justifyContent: "center", alignItems: "center", p: {xs: "0px 10px 25px 0px", md: "0px 10px"} }}
+      >
+        <Box
+          component="img"
+          src={pageNotFound}
+          alt="Carousel 1"
+          sx={{
+            height: { xs: "100%" },
+            width: { xs: "100%", md: "680px" },
+            borderRadius: "30px",
+          }}
+        />
+        <p>
+          <Link to="/">Click here to go back to the landing page</Link>
+        </p>
+      </Stack>
+      <Footer />
     </div>
   );
 }
